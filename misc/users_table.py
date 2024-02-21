0c7551e80f64a68638b3198f44b1e0c9ef5c46a6
VEGAMETA <viperigor12@gmail.com>
2024-02-21 15:17:55 +0300

    fixed comments

--- misc/users_table.py
+++ misc/users_table.py
@@ -12,14 +12,11 @@ def create_and_fill_users_table(app: Flask, db: SQLAlchemy):
     :return:
     """
     with app.app_context():
-        # Creates table if not
         db.create_all()
-        # Check if users are in the table
         if User.query.count() == 0:
-            # Generates data for 5 users
             for _ in range(5):
                 username = f"user{_ + 1}"
                 balance = random.randint(5000, 15000)
                 user = User(username=username, balance=balance)
                 db.session.add(user)
-            db.session.commit()
+        db.session.commit()
